54bb5aae23f0fc21f232b4e940ed59a536c38c52
Cuong <ffgcuong@gmail.com>
2022-07-20 12:01:46 +0700

    design banner for anhkiet

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
-import styled from 'styled-components';
-import Image from 'next/image';
+import React from "react";
+import styled from "styled-components";
+import Image from "next/image";
+import { useViewportSize } from "@mantine/hooks";
 const Wrapper = styled.div``;
 const Content = styled.div`
-  max-width: 800px;
+    max-width: 800px;
 `;
 const ServicesPage = () => {
-  return (
-    <Wrapper>
-      <Content>
-        <Image
-          src="/slider-1.jpg"
-          width={120}
-          height={70}
-          layout="responsive"
-        ></Image>
-      </Content>
+    const { width } = useViewportSize();
+    return (
+        <Wrapper>
+            <Content>
+                <Image
+                    src="/gas-anhkiet/banner/banner-1.svg"
+                    width={100}
+                    height={70}
+                    layout="responsive"
+                    loader={() => {
+                        let result = "/gas-anhkiet/banner/banner-1.svg";
+                        if (width <= 600) {
+                            result = "/gas-anhkiet/banner/banner-1-mobile.svg";
+                        }
 
-      <h3>- Dịch vụ giao gas nhanh </h3>
-      <h3>- Dịch vụ sửa chữa bếp gas tại nhà</h3>
-      <h3>- Dịch vụ tư vấn</h3>
-      <h3>- Dịch vụ đổi trả</h3>
-    </Wrapper>
-  );
+                        return result;
+                    }}
+                ></Image>
+            </Content>
+
+            <h3>- Dịch vụ giao gas nhanh </h3>
+            <h3>- Dịch vụ sửa chữa bếp gas tại nhà</h3>
+            <h3>- Dịch vụ tư vấn</h3>
+            <h3>- Dịch vụ đổi trả</h3>
+        </Wrapper>
+    );
 };
 
 export default ServicesPage;
